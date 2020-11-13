cf2bd17df584b5c9eaa7c57f9344260b0ea67853
Robin De Schepper <robin.deschepper93@gmail.com>
2020-11-13 09:22:27 +0100

    added granule cell selection

--- plots/selection.py
+++ plots/selection.py
@@ -15,6 +15,8 @@ basket_cells = dict(zip(basket_cell_labels, basket_cell_ids))
 stellate_cell_ids = np.array([561, 618])
 stellate_cell_labels = ["High activity", "Low activity"]
 stellate_cells = dict(zip(stellate_cell_labels, stellate_cell_ids))
+granule_cells = np.array([3070, 31681, 3074, 9163, 3083, 11399, 3764, 15288, 5987, 17372])
+granule_cell_order = dict(map(lambda t: (t[1], t[0]), enumerate(granule_cells)))
 
 btn_config = {
     'toImageButtonOptions': {
