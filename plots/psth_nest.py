1bf775a840e6e2d87bd2cefa7e03a410f7724172
Robin De Schepper <robin.deschepper93@gmail.com>
2020-12-10 16:38:25 +0100

    psth_nest now creates both poiss & sync figures

--- plots/psth_nest.py
+++ plots/psth_nest.py
@@ -19,22 +19,41 @@ def plot():
     scaffoldInstance = from_hdf5(network_path)
     config = scaffoldInstance.configuration
     cells = list(scaffoldInstance.get_cell_types())
-    with h5py.File(results_path("results_stim_on_MFs_16075237073796946792023040046.hdf5"), "a") as f:
-        order=dict(record_glomerulus_spikes=0, record_granules_spikes=1, record_golgi_spikes=2, record_pc_spikes=3,
-            record_sc_spikes=4, record_bc_spikes=5)
-        color=dict(record_glomerulus_spikes=config.cell_types["glomerulus"].plotting.color,
-                record_granules_spikes=config.cell_types["granule_cell"].plotting.color,
-                record_golgi_spikes=config.cell_types["golgi_cell"].plotting.color,
-                record_pc_spikes=config.cell_types["purkinje_cell"].plotting.color,
-                record_sc_spikes=config.cell_types["stellate_cell"].plotting.color,
-                record_bc_spikes=config.cell_types["basket_cell"].plotting.color)
+    order = dict(
+        record_glomerulus_spikes=0,
+        record_granules_spikes=1,
+        record_golgi_spikes=2,
+        record_pc_spikes=3,
+        record_sc_spikes=4,
+        record_bc_spikes=5
+    )
+    color=dict(
+        record_glomerulus_spikes=config.cell_types["glomerulus"].plotting.color,
+        record_granules_spikes=config.cell_types["granule_cell"].plotting.color,
+        record_golgi_spikes=config.cell_types["golgi_cell"].plotting.color,
+        record_pc_spikes=config.cell_types["purkinje_cell"].plotting.color,
+        record_sc_spikes=config.cell_types["stellate_cell"].plotting.color,
+        record_bc_spikes=config.cell_types["basket_cell"].plotting.color
+    )
+    figs = {}
+    with h5py.File(results_path("results_NEST_stim_on_MFs_PoissFinal2_finetuning0-02.hdf5"), "a") as f:
         for g in f["/recorders/soma_spikes"].values():
             if g.attrs["label"] not in order:
                 print("Not sorting", g.name, "no order found")
             g.attrs["order"] = order.get(g.attrs["label"], 0)
             g.attrs['color'] = color.get(g.attrs["label"], 0)
-        fig = hdf5_plot_psth(scaffoldInstance, f["/recorders/soma_spikes"], show=False, duration=5)
+        figs["poiss"] = fig = hdf5_plot_psth(scaffoldInstance, f["/recorders/soma_spikes"], show=False, cutoff=300, duration=5)
+        ranges = [[0, 15], [0, 15], [0, 65], [0, 75], [0, 75], [0, 75]]
+        for i in range(len(ranges)):
+            fig.update_yaxes(range=ranges[i], row=i + 1, col=1)
+    with h5py.File(results_path("results_NEST_stim_on_MFs_4syncFinal2_finetuning0-02.hdf5"), "a") as f:
+        for g in f["/recorders/soma_spikes"].values():
+            if g.attrs["label"] not in order:
+                print("Not sorting", g.name, "no order found")
+            g.attrs["order"] = order.get(g.attrs["label"], 0)
+            g.attrs['color'] = color.get(g.attrs["label"], 0)
+        figs["sync"] = fig = hdf5_plot_psth(scaffoldInstance, f["/recorders/soma_spikes"], show=False, cutoff=300, duration=5)
         ranges = [[0, 25], [0, 25], [0, 125], [0, 130], [0, 120], [0, 120]]
         for i in range(len(ranges)):
             fig.update_yaxes(range=ranges[i], row=i + 1, col=1)
-    return fig
+    return figs
