f941f8c5a58afb677c87b555fa5d24228ef86f09
AliceGem <alice.geminiani@polimi.it>
2020-12-14 13:59:36 +0100

    Updated raster python file

--- plots/raster.py
+++ plots/raster.py
@@ -12,8 +12,15 @@ def results_path(*args):
         os.path.dirname(__file__), "..", "results", *args
     )
 
+def select_groups(kv):
+    name, group = kv
+    cell_id = []
+    return group.attrs.get("label", None) == "granule_cell" or name == cell_id
 
 def plot():
-    with h5py.File(results_path("burst_pause/results_poc_1598351489385.hdf5"), "r") as f:
-        fig = hdf5_plot_spike_raster(f["/recorders/soma_spikes"], show=False)
+    with h5py.File(results_path("results_NEST_stim_on_MFs_PoissFinal.hdf5"), "r") as f:
+        print("items ",f["/recorders/soma_spikes"].items())
+        groups = {k: v for k, v in filter(select_groups, f["/recorders/soma_spikes"].items())}
+        print("groups ", groups)
+        fig = hdf5_plot_spike_raster(groups, show=False, cutoff=300)
     return fig
