d24c455a2467b53f74fcd78987a612f379f295aa
Robin De Schepper <robin.deschepper93@gmail.com>
2020-12-14 12:29:49 +0100

    restructured feedforward to a subplot & to report progress

--- plots/feedforward.py
+++ plots/feedforward.py
@@ -1,5 +1,6 @@
 from bsb.core import from_hdf5
 import plotly.graph_objs as go
+from plotly.subplots import make_subplots
 import selection, grc_cloud
 from colour import Color
 import numpy as np, h5py
@@ -15,10 +16,14 @@ def plot_average_all():
     network = from_hdf5("networks/300x_200z.hdf5")
     ampa_currents = []
     gaba_currents = []
-    for file in glob("results/mli/feedforward/*.hdf5"):
+    files = glob("results/mli/feedforward/*.hdf5")
+    ti = len(files)
+    for i, file in enumerate(files):
         with h5py.File(file, "r") as f:
             cells = list(map(int, f["recorders/synapses"].keys()))
-            for cell in cells:
+            tj = len(cells)
+            for j, cell in enumerate(cells):
+                print(f"Run {i+1}/{ti}, cell {j+1}/{tj}")
                 ampa_current = sum(d[3000:] for d in f[f"recorders/synapses/{cell}/current"].values() if d.attrs.get("type", None) == "AMPA")
                 gaba_current = sum(d[3000:] for d in f[f"recorders/synapses/{cell}/current"].values() if d.attrs.get("type", None) == "GABA")
                 ampa_currents.append(ampa_current)
@@ -30,19 +35,16 @@ def plot_average_all():
     norm_gaba = (avg_gaba_current - 0.022) / max(avg_gaba_current[4000:4100] - 0.022)
     corr_x = np.arange(0, len(norm_gaba) / 10, 0.1) - len(norm_gaba) / 20
     corr_y = np.correlate(norm_gaba, norm_ampa, mode="same")
-    corr_fig = go.Figure(go.Scatter(x=corr_x, y=corr_y))
+    corr_fig = make_subplots(rows=1, cols=3, y_title="Current (nA)", x_title="Time (ms)")
+    corr_fig = corr_fig.add_trace(go.Scatter(x=corr_x, y=corr_y), row=1, col=3)
     max_x = np.argmax(corr_y)
     max_y = corr_y[max_x]
-    corr_fig.add_annotation(x=corr_x[max_x], y=max_y, text=round(corr_x[max_x], 2), standoff=3)
+    corr_fig.add_annotation(x=corr_x[max_x], y=max_y, text=round(corr_x[max_x], 2), standoff=3, row=1, col=3)
+    corr_fig.add_trace(go.Scatter(x=time, y=avg_ampa_current), row=1, col=1)
+    corr_fig.add_trace(go.Scatter(x=time, y=avg_gaba_current), row=1, col=1)
+    corr_fig.add_trace(go.Scatter(x=time, y=norm_ampa), row=1, col=2)
+    corr_fig.add_trace(go.Scatter(x=time, y=norm_gaba), row=1, col=2)
+    corr_fig.update_xaxes(range=[350, 450])
+    corr_fig.update_xaxes(range=[-50, 50], row=1, col=3)
 
-    return {
-        "currents_raw": go.Figure([
-            go.Scatter(x=time, y=avg_ampa_current),
-            go.Scatter(x=time, y=avg_gaba_current),
-        ]),
-        "currents_rectified": go.Figure([
-            go.Scatter(x=time, y=norm_ampa),
-            go.Scatter(x=time, y=norm_gaba),
-        ]),
-        "xcorr": corr_fig
-    }
+    return corr_fig
