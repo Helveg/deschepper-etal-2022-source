61872676f9afef1faa9fa1f6a03c98737373a1a4
AliceGem <alice.geminiani@polimi.it>
2020-12-14 13:58:58 +0100

    Multiple runs in PSTH for NEST sims

--- plots/psth_nest.py
+++ plots/psth_nest.py
@@ -4,7 +4,8 @@ from bsb.plotting import hdf5_plot_psth, hdf5_plot_spike_raster
 import numpy as np, h5py
 from scipy import stats
 import plotly.io as pio
-
+from glob import glob
+import itertools
 
 network_path = os.path.join(
     os.path.dirname(__file__), "..", "networks", "300x_200z.hdf5"
@@ -13,6 +14,14 @@ def results_path(*args):
     return os.path.join(
         os.path.dirname(__file__), "..", "results", *args
     )
+
+
+class valueify:
+    def __init__(self, iter):
+        self.iter = iter
+
+    def values(self):
+        return self.iter
 
 
 def plot():
@@ -36,16 +45,39 @@ def plot():
         record_bc_spikes=config.cell_types["basket_cell"].plotting.color
     )
     figs = {}
-    with h5py.File(results_path("results_NEST_stim_on_MFs_PoissFinal2_finetuning0-02.hdf5"), "a") as f:
-        for g in f["/recorders/soma_spikes"].values():
-            if g.attrs["label"] not in order:
-                print("Not sorting", g.name, "no order found")
-            g.attrs["order"] = order.get(g.attrs["label"], 0)
-            g.attrs['color'] = color.get(g.attrs["label"], 0)
-        figs["poiss"] = fig = hdf5_plot_psth(scaffoldInstance, f["/recorders/soma_spikes"], show=False, cutoff=300, duration=5)
-        ranges = [[0, 15], [0, 15], [0, 65], [0, 75], [0, 75], [0, 75]]
+
+    poisson_path = results_path("nest_PoissFinal2")
+    handles = [h5py.File(f, "a") for f in glob(os.path.join(poisson_path, "*.hdf5"))]
+    try:
+        for run_id, handle in enumerate(handles):
+            for g in handle["/recorders/soma_spikes"].values():
+                if g.attrs["label"] not in order:
+                    print("Not sorting", g.name, "no order found")
+                g.attrs["order"] = order.get(g.attrs["label"], 0)
+                g.attrs['color'] = color.get(g.attrs["label"], 0)
+            for ds in handle["/recorders/soma_spikes"].values():
+                # if ds.attrs.get("run_id", None) is not None:
+                #     break
+                ds.attrs["run_id"] = run_id
+            else:
+                continue
+            print("Run ids already detected in:", handle.filename)
+            print("Skipping run id insertion")
+            break
+        figs = {}
+        figs["poiss"] = fig = hdf5_plot_psth(
+            scaffoldInstance,
+            valueify(itertools.chain(*(handle["/recorders/soma_spikes"].values() for handle in handles))),
+            show=False,
+            cutoff=300,
+            duration=5
+        )
+        ranges = [[0, 10], [0, 10], [0, 65], [0, 75], [0, 60], [0, 60]]
         for i in range(len(ranges)):
             fig.update_yaxes(range=ranges[i], row=i + 1, col=1)
+    finally:
+        for handle in handles:
+            handle.close()
     with h5py.File(results_path("results_NEST_stim_on_MFs_4syncFinal2_finetuning0-02.hdf5"), "a") as f:
         for g in f["/recorders/soma_spikes"].values():
             if g.attrs["label"] not in order:
@@ -53,7 +85,7 @@ def plot():
             g.attrs["order"] = order.get(g.attrs["label"], 0)
             g.attrs['color'] = color.get(g.attrs["label"], 0)
         figs["sync"] = fig = hdf5_plot_psth(scaffoldInstance, f["/recorders/soma_spikes"], show=False, cutoff=300, duration=5)
-        ranges = [[0, 25], [0, 25], [0, 125], [0, 130], [0, 120], [0, 120]]
+        ranges = [[0, 30], [0, 27], [0, 125], [0, 130], [0, 120], [0, 120]]
         for i in range(len(ranges)):
             fig.update_yaxes(range=ranges[i], row=i + 1, col=1)
     return figs
