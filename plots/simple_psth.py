eb7951af6d3d0b4b3a83c67c300494442b677288
Robin De Schepper <robin.deschepper93@gmail.com>
2021-03-18 11:52:06 +0100

    changed simple psth to take file args

--- plots/simple_psth.py
+++ plots/simple_psth.py
@@ -7,15 +7,8 @@ from bsb.plotting import hdf5_plot_psth, hdf5_plot_spike_raster
 import numpy as np, h5py
 from scipy import stats
 from glob import glob
-
-
-network_path = os.path.join(
-    os.path.dirname(__file__), "..", "networks", "300x_200z.hdf5"
-)
-def results_path(*args):
-    return os.path.join(
-        os.path.dirname(__file__), "..", "results", *args
-    )
+from ._paths import *
+import selection
 
 
 class valueify:
@@ -25,11 +18,14 @@ class valueify:
     def values(self):
         return self.iter
 
-network = from_hdf5(network_path)
-config = network.configuration
 
-def plot():
-    figs = {}
+def plot(path=None, net_path=None):
+    if path is None:
+        path = results_path("results.hdf5")
+    if net_path is None:
+        net_path = selection.network
+    network = from_hdf5(network_path(net_path))
+    config = network.configuration
     order=dict(mossy_fiber=0, granule_cell=1, golgi_cell=2, purkinje_cell=3, stellate_cell=4, basket_cell=5)
     color=dict(
         mossy_fiber=config.cell_types["glomerulus"].plotting.color,
@@ -39,22 +35,15 @@ def plot():
         stellate_cell=config.cell_types["stellate_cell"].plotting.color,
         basket_cell=config.cell_types["basket_cell"].plotting.color
     )
-    with h5py.File(results_path("results_365b0_poiss.hdf5"), "a") as f:
+    # Check if file exists
+    with h5py.File(path, "r") as f:
+        pass
+    with h5py.File(path, "a") as f:
         for g in f["/recorders/soma_spikes"].values():
         # for g in f["/all"].values():
             if g.attrs["label"] not in order:
                 print("Not sorting", g.name, "no order found")
             g.attrs["order"] = order.get(g.attrs["label"], 0)
             g.attrs['color'] = color.get(g.attrs["label"], 0)
-        figs["poiss"] = hdf5_plot_psth(network, f["/recorders/soma_spikes"], show=False, cutoff=0, duration=5)
-
-
-    with h5py.File(results_path("results_365b0_sync.hdf5"), "a") as f:
-        for g in f["/recorders/soma_spikes"].values():
-        # for g in f["/all"].values():
-            if g.attrs["label"] not in order:
-                print("Not sorting", g.name, "no order found")
-            g.attrs["order"] = order.get(g.attrs["label"], 0)
-            g.attrs['color'] = color.get(g.attrs["label"], 0)
-        figs["sync"] = hdf5_plot_psth(network, f["/recorders/soma_spikes"], show=False, cutoff=0, duration=5)
-    return figs
+        fig = hdf5_plot_psth(network, f["/recorders/soma_spikes"], show=False, cutoff=0, duration=5)
+    return fig
