99c2a5d38e3031bf666475af4cd6ecaf5c70dc85
Robin De Schepper <robin.deschepper93@gmail.com>
2021-04-02 10:05:34 +0200

    switched to `inner_rdf`

--- plots/rdf.py
+++ plots/rdf.py
@@ -2,7 +2,7 @@ from bsb.core import from_hdf5
 from scipy.spatial import KDTree
 from plotly import graph_objs as go
 import numpy as np, pickle
-from radialdf import rdf
+from radialdf import inner_rdf
 
 frozen = False
 from ._paths import *
@@ -20,11 +20,11 @@ def plot(net_path=None):
         dr = 0.5
         if not frozen:
             if ps.tag in ["granule_cell", "glomerulus", "golgi_cell"]:
-                gr = rdf(np.array([[0, 300], [0, 130], [0, 200]]), ps.positions, 30, dr)
+                gr = inner_rdf(np.array([[0, 300], [0, 130], [0, 200]]), ps.positions, 30, dr)
             elif ps.tag in ["basket_cell", "stellate_cell"]:
-                gr = rdf(np.array([[0, 300], [145, 295], [0, 200]]), ps.positions, 30, dr)
+                gr = inner_rdf(np.array([[0, 300], [145, 295], [0, 200]]), ps.positions, 30, dr)
             elif ps.tag == "purkinje_cell":
-                gr = rdf(np.array([[0, 300], [0, 200]]), ps.positions[:, [0, 2]], 30, dr)
+                gr = inner_rdf(np.array([[0, 300], [0, 200]]), ps.positions[:, [0, 2]], 30, dr)
             with open(f"gr_{ps.tag}.pickle", "wb") as f:
                 pickle.dump(gr, f)
         else:
