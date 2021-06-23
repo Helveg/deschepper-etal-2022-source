71b1ffe7125df483648571b7291aba5e34c3b14b
Robin De Schepper <robin.deschepper93@gmail.com>
2021-06-23 13:36:02 +0200

    granule cell ids net8

--- plots/selection.py
+++ plots/selection.py
@@ -22,7 +22,7 @@ basket_cells = dict(zip(basket_cell_labels, basket_cell_ids))
 stellate_cell_ids = np.array([513, 629])
 stellate_cell_labels = ["High activity", "Low activity"]
 stellate_cells = dict(zip(stellate_cell_labels, stellate_cell_ids))
-granule_cells = np.array([3070, 31681, 3074, 9163, 3083, 11399, 3764, 15288, 5987, 17372])
+granule_cells = np.array([[4144, 20916, 3096, 17472, 3141, 14424, 3089, 20089, 3068, 31680])
 granule_cell_order = dict(map(lambda t: (t[1], t[0]), enumerate(granule_cells)))
 granule_cells_balanced = np.array([5987, 6002, 9272, 17285, 17372, 24861, 3764, 3851, 4075,3083, 3114, 3192, 3074, 3076, 3109])
 
