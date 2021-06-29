c42a258f7e08b9c328638c3ff0970e34d2db2c2d
Robin De Schepper <robin.deschepper93@gmail.com>
2021-06-29 17:06:28 +0200

    Created grc+goc oscillations fig

---
+++ plots/oscillations.py
@@ -0,0 +1,20 @@
+from .grc_oscillations import plot as plot_grc_osc
+from .goc_oscillations import plot as plot_goc_osc
+from plotly.subplots import make_subplots
+
+def plot():
+    fig_grc = plot_grc_osc()
+    fig_goc = plot_goc_osc()
+    traces = fig_grc.data + fig_goc.data
+    fig = make_subplots(rows=len(traces), cols=1, y_title="Power [Hz<sup>2</sup>]", x_title="Frequency [Hz]", shared_xaxes=True)
+    for i, t in enumerate(traces):
+        fig.add_trace(t, row=i+1, col=1)
+    fig.update_yaxes(range=[0, 30000], row=1, col=1)
+    fig.update_yaxes(range=[0, 30000], row=2, col=1)
+    fig.update_yaxes(range=[0, 25], row=3, col=1)
+    fig.update_yaxes(range=[0, 25], row=4, col=1)
+    fig.update_layout(showlegend=False)
+    return fig
+
+def meta():
+    return {"width": 1920 / 4, "height": 1920 / 2}
