7f9c8de95c31013f6b4badce48bcf821ae8356ed
Robin De Schepper <robin.deschepper93@gmail.com>
2022-06-21 13:57:30 +0200

    finished start sims

--- plasticity/start_sims.py
+++ plasticity/start_sims.py
@@ -1,23 +1,40 @@
 import subprocess
+import sys
+
+args = sys.argv
 
 takes = {
     "{{pc}}": "pfPC_{}",
     "{{mli}}": "pfMLI_{}",
 }
 procs = []
-for i in range(11):
-    for tag, name in takes.items():
-        # setup plast_{name.format(i)} balanced.hdf5 plast_{name.format(i)}.json 30 13:00:00
-        procs.append(
-            subprocess.Popen(
-                "/store/hbp/ich027/devops/setup_simulation"
-                + f" plast_{name.format(i)} balanced.hdf5 plast_{name.format(i)}.json"
-                + " 30 13:00:00",
-                shell=True
-            )
-        )
 
-print("Processes started.")
+if "sep" in args:
+    for i in range(11):
+        for tag, name in takes.items():
+            # setup plast_{name.format(i)} balanced.hdf5 plast_{name.format(i)}.json 30 13:00:00
+            procs.append(
+                subprocess.Popen(
+                    "/store/hbp/ich027/devops/setup_simulation"
+                    + f" plast_{name.format(i)} balanced.hdf5 plast_{name.format(i)}.json"
+                    + " 30 13:00:00",
+                    shell=True
+                )
+            )
+
+if "nocross" not in args:
+    for i in range(11):
+        for j in range(11):
+                procs.append(
+                    subprocess.Popen(
+                        "/store/hbp/ich027/devops/setup_simulation"
+                        + f" plast_pfMLI_{i}_pfPC_{j} balanced.hdf5 plast_pfMLI_{i}_pfPC_{j}.json"
+                        + " 30 13:00:00",
+                        shell=True
+                    )
+                )
+
+print(len(procs), "processes started.")
 [p.wait() for p in procs]
 print("Processes done.")
 #
