eae5b6d4af93766f915580959e1326d4f3944764
Robin De Schepper <robin.deschepper93@gmail.com>
2022-06-21 13:59:56 +0200

    oops!

--- plasticity/start_sims.py
+++ plasticity/start_sims.py
@@ -12,12 +12,12 @@ procs = []
 if "sep" in args:
     for i in range(11):
         for tag, name in takes.items():
-            # setup plast_{name.format(i)} balanced.hdf5 plast_{name.format(i)}.json 30 13:00:00
+            # setup plast_{name.format(i)} balanced.hdf5 plast_{name.format(i)}.json 13:00:00 30
             procs.append(
                 subprocess.Popen(
                     "/store/hbp/ich027/devops/setup_simulation"
                     + f" plast_{name.format(i)} balanced.hdf5 plast_{name.format(i)}.json"
-                    + " 30 13:00:00",
+                    + " 13:00:00 30",
                     shell=True
                 )
             )
@@ -29,7 +29,7 @@ if "nocross" not in args:
                     subprocess.Popen(
                         "/store/hbp/ich027/devops/setup_simulation"
                         + f" plast_pfMLI_{i}_pfPC_{j} balanced.hdf5 plast_pfMLI_{i}_pfPC_{j}.json"
-                        + " 30 13:00:00",
+                        + " 13:00:00 30",
                         shell=True
                     )
                 )
