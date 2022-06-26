c6ec7ed9afb6d6de2467f13fd7466eea8512aac5
Robin De Schepper <robin.deschepper93@gmail.com>
2022-06-26 17:25:48 +0200

    prepped plastmap for averaging results

--- plots/plastmap.py
+++ plots/plastmap.py
@@ -9,6 +9,9 @@ import re
 import pickle
 import plotly.graph_objs as go
 import numpy as np
+import h5py
+import selection
+from collections import defaultdict
 
 def find(name):
     def finder(a):
@@ -25,42 +28,50 @@ def get_both(name, all_):
 def parse_path(path):
     return ((re.search("pfPC_(\d+)", path) or [None])[0], (re.search("pfMLI_(\d+)", path) or [None])[0])
 
+def cell_freq(path, cells):
+    freq = []
+    with h5py.File(path, "r") as h:
+        for c in cells:
+            t = h[f"recorders/soma_spikes/{c}"][:, 1]
+            freq.append(np.sum((t > 6000) & (t < 6200)) * 5)
+    return (np.mean(freq), np.std(freq))
+
 def plot():
     paths = glob.glob(results_path("plast", "*.hdf5"))
     base_path = next(path for path in paths if "base" in path)
     paths.remove(base_path)
-    if not os.path.exists("plast_freq.pkl"):
-        with open("plast_freq.pkl", "wb") as f:
-            base_freqs = pop_freq.table(base_path, start=6000, end=6100)
-            print(base_freqs)
-            base_freq = get_freq("purkinje_cell", base_freqs)
-            freqs = {parse_path(path): get_both("purkinje_cell", pop_freq.table(path, start=6000, end=6100)) for path in paths}
-            pickle.dump((base_freq, freqs), f)
-    else:
-        with open("plast_freq.pkl", "rb+") as f:
-            base_freq, freqs = pickle.load(f)
-            freqs = {((re.search("\d+", str(pc)) or [None])[0], (re.search("\d+", str(mli)) or [None])[0]): v for (pc, mli), v in freqs.items()}
-            # pickle.dump((base_freq, freqs), f)
-    print(base_freq)
-    mli_x = []
-    mli_y = []
-    pc_x = []
-    pc_y = []
-    for (pc, mli), (freq, std) in freqs.items():
-        print("PC: ", pc)
-        print("MLI:", mli)
-        print("F:  ", freq, "⨦", std)
-        print("+%: ", round(freq / base_freq, 2))
-        if mli is not None:
-            mli_x.append(int(mli))
-            mli_y.append(freq / base_freq)
-        else:
-            pc_x.append(int(pc))
-            pc_y.append(freq / base_freq)
-    xs = np.argsort(mli_x)
-    mli_x = np.array(mli_x)[xs]
-    mli_y = np.array(mli_y)[xs]
-    xs = np.argsort(pc_x)
-    pc_x = np.array(pc_x)[xs]
-    pc_y = np.array(pc_y)[xs]
-    return go.Figure([go.Scatter(x=mli_x, y=mli_y, name="pfMLI"), go.Scatter(x=pc_x, y=pc_y, name="pfPC")])
+    cells = selection.onbeam["purkinje_cell"]
+    base_freq = cell_freq(base_path, cells)[0]
+    freqs = defaultdict(list)
+    for path in paths:
+        parsed = parse_path(path)
+        print("Processing", parsed)
+        freqs[parsed].append(cell_freq(path, cells))
+    freqs = {((re.search("\d+", str(pc)) or [None])[0], (re.search("\d+", str(mli)) or [None])[0]): v for (pc, mli), v in freqs.items()}
+    heatmap = np.full((6, 6), np.nan)
+    for (pc, mli), data in freqs.items():
+        freq = np.mean([f[0] for f in data])
+        std = np.mean([f[1] for f in data])
+        if mli is not None and pc is not None:
+            print("PC: ", pc)
+            print("MLI:", mli)
+            print("F:  ", freq, "⨦", std)
+            print("+%: ", round(freq / base_freq, 2))
+            heatmap[int(mli), int(pc)] = freq / base_freq
+    return go.Figure(
+        go.Heatmap(z=heatmap),
+        layout=dict(
+            xaxis=dict(
+                title="pfPC LTD (AMPA-U downreg.)",
+                tickmode="array",
+                tickvals=list(range(6)),
+                ticktext=[f"{15 * i}%" for i in range(6)],
+            ),
+            yaxis=dict(
+                title="pfMLI LTP (AMPA-U+NMDA-U upreg. -> GABA)",
+                tickmode="array",
+                tickvals=list(range(6)),
+                ticktext=[f"{15 * i}%" for i in range(6)],
+            ),
+        ),
+    )
