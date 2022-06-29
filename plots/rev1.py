c9e1370cc1ff32d87c9698d66359dc1c7c345205
Robin De Schepper <robin.deschepper93@gmail.com>
2022-06-29 14:44:22 +0200

    moved colorbar, fixed whitespace

--- plots/rev1.py
+++ plots/rev1.py
@@ -13,11 +13,14 @@ def plot():
         rows=2,
         cols=2,
         subplot_titles=("Golgi network", "Spike coincidence", "Temporal relation", "Spatial relation"),
+        vertical_spacing=0.05,
+        horizontal_spacing=0.05,
     )
     fig.update_layout(title_text="Golgi millisecond synchrony")
 
     npos = goc_network_graph.plot()["pos"]
     surf = goc_nspos.plot()["surf"]
+    surf.data[0].colorbar = dict(len=0.5, y=0.5)
     sync_hist = goc_sync_hist.plot()
     nsync = goc_nsync.plot()
     transfer(npos, fig, 1, 1)
